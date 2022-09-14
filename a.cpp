dd0036b988eb08d5729f1d175da354666d7c9606
Yelihi <Yelihi19@gmail.com>
2022-09-14 13:58:15 +0900

    First: 알고리즘 교안 공부

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+// 1. 헤더파일입니다. STL 라이브러리를 import합니다. 이 중 bits/stdc++.h는 모든
+// 표준 라이브러리가 포함된 헤더입니다. 그래서 queue나 stack 등을 사용할 때 일일이
+// 거기에 맞춰 #include<stack> 등 입력할 필요가 없습니다. 저것만 입력해놓고 빠르게
+// 알고리즘만 신경 쓰면 됩니다.
+using namespace std;
+string a;
+int main(){
+  cin >> a;
+  cout << a << "\n";
+  return 0;
+}
